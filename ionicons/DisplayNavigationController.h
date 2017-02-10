0fc53c4e6e756ee9e0a5a2f8e550f5b1844f01f9
Admin <admin@Admins-MacBook-Pro-3.local>
2017-02-10 15:43:14 -0800

    Added missing Icon folder

---
+++ ionicons/DisplayNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  DisplayNavigationController.h
+//  projectWells
+//
+//  Created by Admin on 2/7/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DisplayNavigationController : UINavigationController
+
+@end
---
+++ ionicons/DisplayNavigationController.m
@@ -0,0 +1,41 @@
+//
+//  DisplayNavigationController.m
+//  projectWells
+//
+//  Created by Admin on 2/7/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import "DisplayNavigationController.h"
+
+@interface DisplayNavigationController ()
+
+@end
+
+@implementation DisplayNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationBar.translucent = NO;
+    self.navigationBar.barTintColor =[UIColor colorWithRed:97/255. green:179/255. blue:235/255. alpha:1];
+    self.navigationBar.tintColor = [UIColor whiteColor];
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
